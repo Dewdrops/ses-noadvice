e6daf016497666e0586017ea2e93861a8cb14697
Dewdrops <v_v_4474@126.com>
2014-02-28 12:25:49 +0800

    some fix

--- ses-noadvice.el
+++ ses-noadvice.el
@@ -29,101 +29,91 @@
 
 ;;; Code:
 
+(require 'ses)
+
 (defgroup ses-noadvice nil
   "Use ses with less trouble"
   :prefix "ses-noadvice"
   :group 'ses)
 
 (defcustom ses-noadvice-vi-like-binding nil
-  "Whether to add vi-like key binding to ses buffer"
+  "Whether to add vi-like key binding to ses buffer."
   :type 'boolean
   :group 'ses-noadvice)
 
-(eval-after-load 'ses
-  '(progn
 
-     (ses-unload-function)
+(ses-unload-function)
 
-     (define-key ses-mode-print-map (kbd "C-w") 'ses-kill-override)
-     (define-key ses-mode-print-map (kbd "M-w") 'ses-noadvice-copy-region-as-kill)
-     (define-key ses-mode-print-map (kbd "C-y") 'ses-noadvice-yank)
-     (define-key ses-mode-edit-map (kbd "TAB") 'lisp-complete-symbol)
+(define-key ses-mode-print-map (kbd "C-w") 'ses-kill-override)
+(define-key ses-mode-print-map (kbd "M-w") 'ses-noadvice-copy-region-as-kill)
+(define-key ses-mode-print-map (kbd "C-y") 'ses-noadvice-yank)
+(define-key ses-mode-edit-map (kbd "TAB") 'lisp-complete-symbol)
 
-     (when ses-noadvice-vi-like-binding
-       (define-key ses-mode-print-map (kbd "g") 'ses-jump)
-       (define-key ses-mode-print-map (kbd "j") 'next-line)
-       (define-key ses-mode-print-map (kbd "k") 'previous-line)
-       (define-key ses-mode-print-map (kbd "h") 'backward-char)
-       (define-key ses-mode-print-map (kbd "l") 'forward-char)
-       (define-key ses-mode-print-map (kbd "o") 'ses-insert-row)
-       (define-key ses-mode-print-map (kbd "O") 'ses-insert-column)
-       (define-key ses-mode-print-map (kbd "u") 'undo)
-       (define-key ses-mode-print-map (kbd "d") 'ses-clear-cell-forward)
-       (define-key ses-mode-print-map (kbd "v") 'set-mark-command)
-       (define-key ses-mode-print-map (kbd "s") 'ses-kill-override)
-       (define-key ses-mode-print-map (kbd "y") 'ses-noadvice-copy-region-as-kill)
-       (define-key ses-mode-print-map (kbd "p") 'ses-noadvice-yank)
-       (define-key ses-mode-print-map (kbd "P") 'ses-read-cell-printer))
+(when ses-noadvice-vi-like-binding
+  (define-key ses-mode-print-map (kbd "g") 'ses-jump)
+  (define-key ses-mode-print-map (kbd "j") 'next-line)
+  (define-key ses-mode-print-map (kbd "k") 'previous-line)
+  (define-key ses-mode-print-map (kbd "h") 'backward-char)
+  (define-key ses-mode-print-map (kbd "l") 'forward-char)
+  (define-key ses-mode-print-map (kbd "o") 'ses-insert-row)
+  (define-key ses-mode-print-map (kbd "O") 'ses-insert-column)
+  (define-key ses-mode-print-map (kbd "u") 'undo)
+  (define-key ses-mode-print-map (kbd "d") 'ses-clear-cell-forward)
+  (define-key ses-mode-print-map (kbd "v") 'set-mark-command)
+  (define-key ses-mode-print-map (kbd "s") 'ses-kill-override)
+  (define-key ses-mode-print-map (kbd "y") 'ses-noadvice-copy-region-as-kill)
+  (define-key ses-mode-print-map (kbd "p") 'ses-noadvice-yank)
+  (define-key ses-mode-print-map (kbd "P") 'ses-read-cell-printer))
 
-     (defun ses-noadvice-copy-region-as-kill (beg end)
-       "Reimplement ses' copy-region-as-kill advice use function"
-       (interactive "r")
-       (when (> beg end)
-         (let ((temp beg))
-           (setq beg end
-                 end temp)))
-       (if (not (and (eq (get-text-property beg 'read-only) 'ses)
-                     (eq (get-text-property (1- end) 'read-only) 'ses)))
-           (copy-region-as-kill beg end)
-         (kill-new (ses-copy-region beg end))
-         (if transient-mark-mode
-             (setq deactivate-mark t))
-         nil))
+(defun ses-noadvice-copy-region-as-kill (beg end)
+  "Reimplement ses' `copy-region-as-kill' advice use function."
+  (interactive "r")
+  (when (> beg end)
+    (let ((temp beg))
+      (setq beg end
+            end temp)))
+  (if (not (and (eq (get-text-property beg 'read-only) 'ses)
+                (eq (get-text-property (1- end) 'read-only) 'ses)))
+      (copy-region-as-kill beg end)
+    (kill-new (ses-copy-region beg end))
+    (if transient-mark-mode
+        (setq deactivate-mark t))
+    nil))
 
-     (defun ses-kill-override (beg end)
-       "Reimplement ses-kill-override without lookup key"
-       (interactive "r")
-       (ses-check-curcell 'needrange)
-       ;; For some reason, the text-read-only error is not caught by `delete-region',
-       ;; so we have to use subterfuge.
-       (let ((buffer-read-only t))
-         (1value (condition-case x
-                     (noreturn (kill-region beg end))
-                   (buffer-read-only nil)))) ; The expected error.
-       ;; Because the buffer was marked read-only, the kill command turned itself
-       ;; into a copy.  Now we clear the cells or signal the error.  First we check
-       ;; whether the buffer really is read-only.
-       (barf-if-buffer-read-only)
-       (ses-begin-change)
-       (ses-dorange ses--curcell
-         (ses-clear-cell row col))
-       (ses-jump (car ses--curcell)))
+(defun ses-kill-override (beg end)
+  "Reimplement `ses-kill-override' without lookup key."
+  (interactive "r")
+  (ses-noadvice-copy-region-as-kill beg end)
+  (barf-if-buffer-read-only)
+  (ses-begin-change)
+  (ses-dorange ses--curcell
+    (ses-clear-cell row col))
+  (ses-jump (car ses--curcell)))
 
-     (defun ses-noadvice-yank (&optional arg)
-       "Reimplement ses' yank advice use function"
-       (interactive "*P")
-       (let ((x-select-enable-primary nil))
-         (if (not (eq (get-text-property (point) 'keymap) 'ses-mode-print-map))
-             (yank arg)
-           (ses-check-curcell 'end)
-           (push-mark (point))
-           (let ((text (current-kill (cond
-                                      ((listp arg) 0)
-                                      ((eq arg '-) -1)
-                                      (t (1- arg))))))
-             (or (ses-yank-cells text arg)
-                 (ses-yank-tsf text arg)
-                 (ses-yank-one (ses-yank-resize 1 1)
-                               text
-                               0
-                               (if (memq (aref text (1- (length text))) '(?\t ?\n))
-                                   ;; Just one cell --- delete final tab or newline.
-                                   (1- (length text)))
-                               arg)))
-           (if (consp arg)
-               (exchange-point-and-mark)))))
+(defun ses-noadvice-yank (&optional arg)
+  "Reimplement ses' yank advice use function."
+  (interactive "*P")
+  (let ((x-select-enable-primary nil))
+    (if (not (eq (get-text-property (point) 'keymap) 'ses-mode-print-map))
+        (yank arg)
+      (ses-check-curcell 'end)
+      (push-mark (point))
+      (let ((text (current-kill (cond
+                                 ((listp arg) 0)
+                                 ((eq arg '-) -1)
+                                 (t (1- arg))))))
+        (or (ses-yank-cells text arg)
+            (ses-yank-tsf text arg)
+            (ses-yank-one (ses-yank-resize 1 1)
+                          text
+                          0
+                          (if (memq (aref text (1- (length text))) '(?\t ?\n))
+                              ;; Just one cell --- delete final tab or newline.
+                              (1- (length text)))
+                          arg)))
+      (if (consp arg)
+          (exchange-point-and-mark)))))
 
-     ))
 
 (provide 'ses-noadvice)
-;;; ses-noadvice-setting.el ends here
+;;; ses-noadvice.el ends here
